548f5c6a73417adb27d3abf13350ffe904614e25
macOS <macos@Quangs-MacBook-Pro.local>
2025-09-21 21:57:21 +0700

    fix(heroku): remove vite from prod runtime

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import "./env";
 
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { serveStatic, log } from "./vite";
 
 const app = express();
 app.use(express.json());
@@ -53,6 +53,7 @@ app.use((req, res, next) => {
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
   if (app.get("env") === "development") {
+    const { setupVite } = await import("./vite-dev.js");
     await setupVite(app, server);
   } else {
     serveStatic(app);
